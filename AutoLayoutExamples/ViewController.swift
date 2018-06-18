9b3fb9f8ff85da75f9d38a080a9e1a26b3d267c4
Andres Muñoz <andres.munoz@orbis.com.pe>
2018-06-18 16:59:53 -0500

    feat(demo): Adding single autolayout demo

--- AutoLayoutExamples/ViewController.swift
+++ AutoLayoutExamples/ViewController.swift
@@ -13,22 +13,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        centerWithFrame()
+        //centerWithFrame()
+        centerWithConstraints()
     }
 
 
     func centerWithFrame() {
-        let btnWidth = myBtn.frame.width + 50
+        let btnWidth = myBtn.frame.width + 200
         if let superview = myBtn.superview {
             let x = (superview.frame.width / 2) - (btnWidth / 2)
             let y = (superview.frame.height / 2) - (myBtn.frame.height / 2)
             let position = CGRect(x: x, y: y, width: btnWidth, height: myBtn.frame.height)
             myBtn.frame = position
-            myBtn.setTitle("x:\(x), y:\(y)", for: .normal)
+            //myBtn.setTitle("x:\(x), y:\(y)", for: .normal)
         }
     }
 
     func centerWithConstraints() {
-        //
+        let btnWidth = myBtn.frame.width + 200
+        myBtn.translatesAutoresizingMaskIntoConstraints = false
+        if let superview = myBtn.superview {
+            myBtn.widthAnchor.constraint(equalToConstant: btnWidth).isActive = true
+            myBtn.centerXAnchor.constraint(equalTo: superview.centerXAnchor).isActive = true
+            myBtn.centerYAnchor.constraint(equalTo: superview.centerYAnchor).isActive = true
+            //myBtn.setTitle("x: \(myBtn.frame.origin.x), y:\(myBtn.frame.origin.y)", for: .normal)
+        }
     }
 }
+
